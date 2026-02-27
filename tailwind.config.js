d38a65ecc0c968d1e26d8563901fe59fe8687662
JayLazaro <62410276+JayLazaro@users.noreply.github.com>
2026-02-27 13:16:29 +1100

    Fix Tailwind config + add portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./*.html",           // Scans ALL HTML files in root
+    "./src/**/*.html",    // Scans HTML in src folder
+    "./index.html"        // Your main file
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
